a400e0b240d1646adf0dc7da14a522ec05c4762b
marcelo <marceloaazeredo@gmail.com>
2021-03-17 21:33:45 -0300

    ADDED: Two-Fer exercise solution

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class TwoFer
+{
+    // In order to get the tests running, first you need to make sure the Speak method 
+    // can be called both without any arguments and also by passing one string argument.
+    public static string Speak(String name = "you")
+    {
+        //throw new NotImplementedException("You need to implement this function.");
+        if (name is null)
+        {
+            name = "you";
+        }
+
+        return ("One for " + name + ", one for me.");
+    }
+
+}
