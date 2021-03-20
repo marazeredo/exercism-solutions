a93627245e22737b4167b89794d4cf3ecada5fd5
marcelo <marceloaazeredo@gmail.com>
2021-03-20 12:16:21 -0300

    ADDED: Resistor color exercise's solution

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        int aux = 0;
+        switch (color)
+        {
+            case "black":
+                aux = 0;
+                break;
+            case "brown":
+                aux = 1;
+                break;
+            case "red":
+                aux = 2;
+                break;
+            case "orange":
+                aux = 3;
+                break;
+            case "yellow":
+                aux = 4;
+                break;
+            case "green":
+                aux = 5;
+                break;
+            case "blue":
+                aux = 6;
+                break;
+            case "violet":
+                aux = 7;
+                break;
+            case "gray":
+                aux = 8;
+                break;
+            case "white":
+                aux = 9;
+                break;
+        }
+        return aux;
+        //return code;
+    }
+    public static string[] Colors()
+    {
+        string[] color = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+        return color;
+    }
+}
