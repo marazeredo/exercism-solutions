7a35a37609af86fe3fbe18c5de10a8dcc0f1aa9d
marcelo <marceloaazeredo@gmail.com>
2021-03-20 12:38:43 -0300

    CHANGED: Resistor colors exercise's solution using Array.IndexOf

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -4,41 +4,42 @@ public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        int code = 0;
-        switch (color)
-        {
-            case "black":
-                code = 0;
-                break;
-            case "brown":
-                code = 1;
-                break;
-            case "red":
-                code = 2;
-                break;
-            case "orange":
-                code = 3;
-                break;
-            case "yellow":
-                code = 4;
-                break;
-            case "green":
-                code = 5;
-                break;
-            case "blue":
-                code = 6;
-                break;
-            case "violet":
-                code = 7;
-                break;
-            case "gray":
-                code = 8;
-                break;
-            case "white":
-                code = 9;
-                break;
-        }
-        return code;
+        return Array.IndexOf(Colors(), color);
+        // int code = 0;
+        // switch (color)
+        // {
+        //     case "black":
+        //         code = 0;
+        //         break;
+        //     case "brown":
+        //         code = 1;
+        //         break;
+        //     case "red":
+        //         code = 2;
+        //         break;
+        //     case "orange":
+        //         code = 3;
+        //         break;
+        //     case "yellow":
+        //         code = 4;
+        //         break;
+        //     case "green":
+        //         code = 5;
+        //         break;
+        //     case "blue":
+        //         code = 6;
+        //         break;
+        //     case "violet":
+        //         code = 7;
+        //         break;
+        //     case "gray":
+        //         code = 8;
+        //         break;
+        //     case "white":
+        //         code = 9;
+        //         break;
+        // }
+        // return code;
     }
     public static string[] Colors()
     {
