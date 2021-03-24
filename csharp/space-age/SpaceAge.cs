7fad83f0fb3dc7aa944061dd61b9bff0feab0182
marcelo <marceloaazeredo@gmail.com>
2021-03-23 21:14:14 -0300

    CHANGED: including earthSeconds variable

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -3,6 +3,7 @@ using System;
 public class SpaceAge
 {
     int Seconds;
+    double earthSeconds = 31557600.00;
     public SpaceAge(int seconds)
     {
         Seconds = seconds;
@@ -10,49 +11,49 @@ public class SpaceAge
 
     public double OnEarth()
     {
-        double earthAge = Seconds / 31557600.00;
+        double earthAge = Seconds / earthSeconds;
         return earthAge;
     }
 
     public double OnMercury()
     {
-        double mercuryAge = (Seconds / 31557600.00) / 0.2408467;
+        double mercuryAge = (Seconds / earthSeconds) / 0.2408467;
         return mercuryAge;
     }
 
     public double OnVenus()
     {
-        double venusAge = (Seconds / 31557600.00) / 0.61519726;
+        double venusAge = (Seconds / earthSeconds) / 0.61519726;
         return venusAge;
     }
 
     public double OnMars()
     {
-        double marsAge = (Seconds / 31557600.00) / 1.8808158;
+        double marsAge = (Seconds / earthSeconds) / 1.8808158;
         return marsAge;
     }
 
     public double OnJupiter()
     {
-        double jupterAge = (Seconds / 31557600.00) / 11.862615;
+        double jupterAge = (Seconds / earthSeconds) / 11.862615;
         return jupterAge;
     }
 
     public double OnSaturn()
     {
-        double saturnoAge = (Seconds / 31557600.00) / 29.447498;
+        double saturnoAge = (Seconds / earthSeconds) / 29.447498;
         return saturnoAge;
     }
 
     public double OnUranus()
     {
-        double uranusAge = (Seconds / 31557600.00) / 84.016846;
+        double uranusAge = (Seconds / earthSeconds) / 84.016846;
         return uranusAge;
     }
 
     public double OnNeptune()
     {
-        double neptuneAge = (Seconds / 31557600.00) / 164.79132;
+        double neptuneAge = (Seconds / earthSeconds) / 164.79132;
         return neptuneAge;
     }
 }
