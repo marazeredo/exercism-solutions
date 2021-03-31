1c8d8c8388282d4991679b0d78ff38606dd7c702
marazeredo <marceloaazeredo@gmail.com>
2021-03-31 18:24:57 -0300

    CHANGED: identation using pointer

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -9,5 +9,6 @@ public class HighScores
     public List<int> Scores() => _list;
     public int Latest() => _list.Last();
     public int PersonalBest() => _list.Max();
+    
     public List<int> PersonalTopThree() => _list.OrderByDescending(x => x).Take(3).ToList();
 }
