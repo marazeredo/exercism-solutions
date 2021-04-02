489982f2d1b89421c59b2000d836d1152539a94d
marazeredo <marceloaazeredo@gmail.com>
2021-04-02 13:40:08 -0300

    CHANGED: Hudson's issues

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -2,48 +2,8 @@
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
-    {
-        return Array.IndexOf(Colors(), color);
-        // int code = 0;
-        // switch (color)
-        // {
-        //     case "black":
-        //         code = 0;
-        //         break;
-        //     case "brown":
-        //         code = 1;
-        //         break;
-        //     case "red":
-        //         code = 2;
-        //         break;
-        //     case "orange":
-        //         code = 3;
-        //         break;
-        //     case "yellow":
-        //         code = 4;
-        //         break;
-        //     case "green":
-        //         code = 5;
-        //         break;
-        //     case "blue":
-        //         code = 6;
-        //         break;
-        //     case "violet":
-        //         code = 7;
-        //         break;
-        //     case "gray":
-        //         code = 8;
-        //         break;
-        //     case "white":
-        //         code = 9;
-        //         break;
-        // }
-        // return code;
-    }
-    public static string[] Colors()
-    {
-        string[] color = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
-        return color;
-    }
+    public static int ColorCode(string color) =>
+        Array.IndexOf(Colors(), color);
+    public static string[] Colors() =>
+        new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
 }
