6bebe2e319f9b6c6e4aeb20b17b9a21c62b0dd89
marazeredo <marceloaazeredo@gmail.com>
2021-04-02 13:11:29 -0300

    ADDED: Hamming Distance exercise solution

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,8 +4,25 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-    
+        int hammingDistance = 0;
+        char[] fS = firstStrand.ToCharArray();
+        char[] sS = secondStrand.ToCharArray();
 
-       return 0;
+        if (firstStrand.Length == secondStrand.Length)
+        {
+            for (int ctr = 0; ctr < fS.Length; ctr++)
+            {
+                if (fS[ctr] != sS[ctr])
+                {
+                    hammingDistance++;
+                }
+            }
+            return hammingDistance;
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+
     }
 }
