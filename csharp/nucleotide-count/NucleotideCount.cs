4d92cb3929e23a5029766fc1770230d930dbe37b
marcelo <marceloaazeredo@gmail.com>
2021-11-04 21:44:54 -0300

    ADDED: nucleotide exercism solution (not working for invalid string

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,65 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
+        IDictionary<char, int> DNA = new Dictionary<char, int>();
+
+        int count_A = 0;
+        int count_C = 0;
+        int count_G = 0;
+        int count_T = 0;
+        bool invalid = false;
+
+        int DNA_length = sequence.Length;
+        char[] aux = sequence.ToCharArray();
+
+        for (int i = 0; i < DNA_length; i++)
+        {
+            switch (aux[i])
+            {
+                case 'A':
+                {
+                    count_A++;
+                    break;
+                    }
+
+                case 'C':
+                {
+                    count_C++;
+                    break;
+                    }
+
+                case 'G':
+                {
+                    count_G++;
+                    break;
+                    }
+
+                case 'T':
+                {
+                    count_T++;
+                    break;
+                    }
+
+                default:
+                    invalid = true;
+                    break;
+            }
+        }
+
+        DNA.Add('A', count_A);
+        DNA.Add('C', count_C);
+        DNA.Add('G', count_G);
+        DNA.Add('T', count_T);
+        //DNA.Add('I', count_invalid);
+
+        if (!invalid)
+        {
+            return DNA;
+        }
+        else
+        {
+           // return ;
+        }
         
     }
 }
